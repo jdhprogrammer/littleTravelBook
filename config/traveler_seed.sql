79ef327556bba011b24d369b491979fc29b306ee
David Harris <jdh.programmer@gmail.com>
2021-01-08 17:56:41 -0600

    pushing to new heroku

---
+++ config/traveler_seed.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM pbslnbp7cahe1jf4.Travelers
+LIMIT 0, 50
+
+-- Date: 2021-01-08 17:46
+*/
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (1,'Shani','shani2@shani.com','$2a$10$zFFwT9JHWHvWKA22oNdPqOYnyjHlR2OocP2Ykl.s03Kl.ThhjUitK','2020-12-16 14:12:30','2020-12-16 14:12:30');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (2,'David Harris','jdh.programmer@gmail.com','$2a$10$N4z77sdfGaif7S3QGj..xexdkFT7MtJhBPAAUXsCZsMzuu/uP805m','2020-12-16 14:34:06','2020-12-16 14:34:06');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (3,'Michael','griffithmichael@me.com','$2a$10$290a4LO2TPPORL8/mSjnuOplWZYvFtsfJWrOW8/tTQbGTYyRDXUWC','2020-12-16 23:25:26','2020-12-16 23:25:26');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (4,'JD Harris','david@gmail.com','$2a$10$ZkGL3tLfIRmTuplR98qO7exqiveMbq0qe3A/JB77gXfmv3kQW5hhu','2020-12-17 00:40:51','2020-12-17 00:40:51');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (5,'JuJu SmithSchuster','JUJU@gmail.com','$2a$10$Qwbhh1d9dQb2AecahIhFS.3wXy4kBwTmzioCDsLzZPHJn5hHBjm2i','2020-12-17 01:42:57','2020-12-17 01:42:57');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (90,'James Harris','james@gmail.com','$2a$10$d0PtFOI1F/ydO3Wlq5cHBuYKRDH.AcOIKj1tItvUWmKE1gzqS7cAq','2020-12-15 16:43:30','2020-12-15 16:43:30');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (91,'Michael Griffith','Mike@aol.com','$2a$10$kGB7/bLy027WnVNIUKRhP.APwevQS8yR/2QD.WMnKWQ8GBDeHgEqm','2020-12-15 18:38:56','2020-12-15 18:38:56');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (93,'Abuelia Clemen Rodriguez','abuelita@gmail.com','$2a$10$ZNjevLfnAhlUlNSO7JeEjuibRyNII0VkcHvCaZoLTpmV5rgODoNvi','2020-12-16 01:36:25','2020-12-16 01:36:25');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (94,'lilwayne','eeeee@ghettoboys.com','$2a$10$ndjV8NR9VzVx1k1i1wyTgOepjKFm0easn8VVwy2TLt0bqAzxyHfNi','2020-12-15 03:51:30','2020-12-15 03:51:30');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (95,'captain jack','jackieboy@yoho.com','$2a$10$zfPXxBclnBcVnbsWxXyJvenOSmA.JVchmSGKJaEvzn51l4bInbkmS','2020-12-15 03:14:08','2020-12-15 03:14:08');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (96,'Christopher Griffith','michael@met.com','$2a$10$9Rv/nxlaa8hCGvzc9mmPxOvnm/aGVKRdNTM9rKL.toFvOC3oWU/K.','2020-12-17 15:09:37','2020-12-17 15:09:37');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (97,'Debbie','griffith1963@yahoo.com','$2a$10$.CkdqoxAQZ34ITOu5ooV3eWqDKnydHne9USgcs1x1BIihy53QCOKe','2020-12-17 20:14:07','2020-12-17 20:14:07');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (98,'Shelby Griffith','shelby746@hotmail.com','$2a$10$Et4me09EdZG8O8nTT0j4b.Ang73d2FBzG4v1EpCo1pInlolz9Oqdm','2020-12-17 20:16:14','2020-12-17 20:16:14');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (99,'Peyton','pmix330@gmail.com','$2a$10$JanH9FOyW1W5/1uNAaIl8eZ3.dhA07NesVo1PYlrGxNPxe7XLQB66','2020-12-18 22:10:25','2020-12-18 22:10:25');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (100,'Elisha','missgomez1886@gmail.com','$2a$10$R7wohCp6YwuUvq6vYa5eXu0JQ0nI9W.gyotvqy9rG.VmP5q7xYsE2','2020-12-24 08:00:38','2020-12-24 08:00:38');
+INSERT INTO `Travelers` (`id`,`name`,`email`,`password`,`createdAt`,`updatedAt`) VALUES (101,'Bobby','bobby@bobby.com','$2a$10$j6WzdbV4CkqW2BKm8591a.aqG7cZXww3zdEERRNRU3N9G8bq69dnu','2020-12-31 01:03:21','2020-12-31 01:03:21');
